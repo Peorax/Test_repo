533a8ccacde1a88277131392eb3b79a901d05429
Peorax <swrdwng@gmail.com>
2025-04-19 22:40:31 +0300

    001

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Second commit!");
 
-        Cat firstCat = new Cat("Борис","бежевый",2);
-        Cat secondCat = new Cat("Мурзик","черно-белый",3);
+        Cat firstCat = new Cat("Boris","Bezhe",2);
+        Cat secondCat = new Cat("Myrzik","Gray",3);
 
         System.out.println("Данные первого кота : " + firstCat.getName() + " , "
                 + firstCat.getColor() + " , " + firstCat.getAge());
